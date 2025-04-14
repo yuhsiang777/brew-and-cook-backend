9c1bcf462aeaeaca8f20878f9cb2c56e856cecf7
DESKTOP-TL98MIK\User <da0226894713@gmail.com>
2025-04-15 00:36:04 +0800

    新增"Products 產品資料表"欄位

--- entities/Products.js
+++ entities/Products.js
@@ -17,10 +17,9 @@ module.exports = new EntitySchema({
       unique: true,
       nullable: false
     },
-    description: {
-      type: "varchar",
-      length: 500,
-      nullable: false
+    category_id: {
+      type: "uuid",
+      nullable: true,
     },
     price: {
       type: "decimal",
@@ -28,20 +27,45 @@ module.exports = new EntitySchema({
       scale: 2,
       nullable: false,
     },
-    category_id: {
-      type: "uuid",
+    colors: {
+      type: "simple-json",
+      nullable: true,
+      comment: "產品可選擇的顏色，例如：[\"紅\", \"白\", \"黑\"]"
+    },
+    specs: {
+      type: "simple-json",
+      nullable: true,
+      comment: "產品規格，例如：{\"容量\":\"500ml\", \"功率\":\"1000W\"}"
+    },
+    size: {
+      type: "varchar",
+      length: 50,
       nullable: true,
     },
-    weight: {
-      type: "decimal",
-      precision: 10,
-      scale: 2,
-      nullable: false,
+    description: {
+      type: "varchar",
+      length: 500,
+      nullable: false
     },
-    dimensions: {
+    materials: {
+      type: "varchar",
+      length: 100,
+      nullable: true,
+    },
+    warranty: {
       type: "varchar",
       length: 20,
-      nullable: false
+      nullable: true,
+    },
+    origin: {
+      type: "varchar",
+      length: 20,
+      nullable: true,
+    },
+    notices: {
+      type: "simple-json",
+      nullable: true,
+      comment: "注意事項，例如：[\"請遠離火源\", \"不可水洗\"]"
     },
     isActive: {
       type: "boolean",
@@ -70,7 +94,7 @@ module.exports = new EntitySchema({
     },
     productTag: {
       target: "ProductTag", // 使用中间表 ProductTag 实体
-      type: "one-to-many", 
+      type: "one-to-many",
       cascade: true, // 允許自動保存中間表記錄
     }
   }

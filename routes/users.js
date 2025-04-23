2ae996cb3036106876b56a189872338b7cc6b079
yuhsiang777 <da0226894713@gmail.com>
2025-04-23 17:18:42 +0800

    更新 users

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { register } = require('../controllers/userController')
+const { register, signIn } = require('../controllers/userController')
 
 // 註冊 API
 router.post('/sign-up', register)
-router.post('/sign-in',register)
+router.post('/sign-in', signIn)
 
 module.exports = router
